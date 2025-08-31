183724d27263aea3dad57fb5f7f21b1524d874f8
housewoods <max.hauswald@tutanota.com>
2025-08-31 10:17:50 +0200

    code refractured

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         content = f.read()
     return content
 
-def count_words(string):
-    num_words = len(string.split())
-    return num_words
+
 
 def main():
     content = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,6 @@
+def count_words(string):
+    num_words = len(string.split())
+    return num_words
+
+def char_counter(string):
+    
