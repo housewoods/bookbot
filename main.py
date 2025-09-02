5070ee02753fbfd581b994b858369a990d3dadd0
housewoods <max.hauswald@tutanota.com>
2025-09-02 17:36:29 +0200

    variable  filepath added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import count_words
 from stats import char_counter
 from stats import char_presorter
+import sys
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,20 +12,24 @@ def get_book_text(filepath):
 
 
 def main():
-    path = "books/frankenstein.txt"
-    content = get_book_text(path)
-    num_words = count_words(content)
-    print("============ BOOKBOT ============")
-    print(f" Analyzing book found at {path}....")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    characters = char_counter(content)
-    char_presorted = char_presorter(characters)
-    for element in char_presorted:
-        character = element["Character"]
-        num = element["num"]
-        print(f"{character}: {num}")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:        
+        path = sys.argv[1]
+        content = get_book_text(path)
+        num_words = count_words(content)
+        print("============ BOOKBOT ============")
+        print(f" Analyzing book found at {path}....")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        characters = char_counter(content)
+        char_presorted = char_presorter(characters)
+        for element in char_presorted:
+            character = element["Character"]
+            num = element["num"]
+            print(f"{character}: {num}")
 
 
 main()
